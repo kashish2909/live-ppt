1c2026d4ed95668a22c816a240faa4d9f9434936
kashish2909 <bansal.kashish6@gmail.com>
2019-02-17 20:32:45 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var socket=require('socket.io');
+var app=express();
+
+var server=app.listen(4000,function () {
+console.log('listening 4000');
+})
+
+
+app.use(express.static('public/reveal.js'));
+
+var io=socket(server);
+
+io.on('connection',function(socket){
+    console.log('made socket connection');
+
+    socket.on('change',function(data)
+    {
+        console.log("on server side");
+        io.sockets.emit('change',data);
+    });
+});
