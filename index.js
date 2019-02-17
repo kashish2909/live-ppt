07d2d7099c1c19ff291946d7081cbd2e0561bb9d
kashish2909 <bansal.kashish6@gmail.com>
2019-02-17 21:22:30 +0530

    bugs removed

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app=express();
 
 const PORT= process.env.PORT || 4000;
 var server=app.listen(PORT,function () {
-console.log('listening ${PORT}');
+//console.log(`listening ${ PORT }`);
 });
 
 
@@ -13,11 +13,11 @@ app.use(express.static('public/reveal.js'));
 var io=socket(server);
 
 io.on('connection',function(socket){
-    console.log('made socket connection');
+    //console.log('made socket connection');
 
     socket.on('change',function(data)
     {
-        console.log("on server side");
+        //console.log("on server side");
         io.sockets.emit('change',data);
     });
 });
